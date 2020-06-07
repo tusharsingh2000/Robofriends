413deab8c7d7b05fe4cdbb06fa15ec2db36f50ce
tusharsingh2000 <tusharsinghynr@gmail.com>
2020-06-07 16:51:26 +0530

    Updates

---
+++ precache-manifest.5d109a22dc64af7003cdceaa863fa1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b370815a8fee697fe034ebdf47d1c672",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8c29e51b9e7d717d006d",
+    "url": "/Robofriends/static/css/main.9e6e999d.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/Robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c29e51b9e7d717d006d",
+    "url": "/Robofriends/static/js/main.2229e765.chunk.js"
+  },
+  {
+    "revision": "4eac723178dd1eab715f",
+    "url": "/Robofriends/static/js/runtime-main.33ca4b66.js"
+  }
+]);
